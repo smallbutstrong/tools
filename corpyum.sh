5e5b912b6225ccea814dc9caf993dbf3c6c5f2c1
james.xu <xuzq@yuntongxun.com>
2018-07-18 22:01:32 +0800

    judge version

--- corpyum.sh
+++ corpyum.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-curl -O https://copr.fedorainfracloud.org/coprs/librehat/shadowsocks/repo/epel-7/librehat-shadowsocks-epel-7.repo -o /etc/yum.repos.d/librehat-shadowsocks-epel-7.repo
-yum install -y shadowsocks-libev
+version=`rpm -q centos-release | cut -d- -f 3`
+printf "install repo for shadowsocks..."
+sudo wget -v https://copr.fedorainfracloud.org/coprs/librehat/shadowsocks/repo/epel-$version/librehat-shadowsocks-epel-$version.repo -o /etc/yum.repos.d/librehat-shadowsocks-epel.repo
+printf "finished\n"
+#yum install -y shadowsocks-libev
