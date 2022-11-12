b46c09938c9869dbd3c449dac124aa686b074046
james <james.xuzhiqiang@google.com>
2022-11-12 11:40:38 +0700

    add region for install docker

--- install_docker.sh
+++ install_docker.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
+IS_INCHINA=1
+ret=$(curl -s ipinfo.io)
+if [[ ! $ret =~ .*"CN".* ]] ;then
+   IS_INCHINA=0
+fi
+
 ret=$(rpm -qa containerd.io)
 if [ ! -z $ret ] ;then
    echo "docker already exists!"
-   exit
+   #exit
 fi
 
-exit
 #移除旧的版本
 yum remove docker \
     docker-client \
@@ -19,18 +24,26 @@ yum remove docker \
 
 #安装docker仓库
 yum install -y yum-utils 
-yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+if [ $IS_INCHINA -eq 1 ] ; then
+   yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+else
+   yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+fi
 
 #安装新版本docker
-yum install docker-ce docker-ce-cli containerd.io -y
+yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 #配置阿里云镜像
+if [ $IS_INCHINA -eq 1 ] ; then
 mkdir -p /etc/docker
 tee /etc/docker/daemon.json <<-'EOF'
 {
-  "registry-mirrors": ["https://vvew714u.mirror.aliyuncs.com"]
+    "registry-mirrors": ["https://vvew714u.mirror.aliyuncs.com"]
 }
 EOF
+fi
+
+
 
 #设置服务自启动
 systemctl enable docker
---
+++ run_v2ray.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+docker run -d --name v2ray -p 127.0.0.1:18000:1080 --restart always   smallbutstrong/v2ray
+
